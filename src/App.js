2d580c1dc8a1b41f7bb89f56acba43711a625932
meisammofidi <meisam.mofidi@yahoo.com>
2021-06-02 23:38:04 -0400

    Stored  data into local storage and load them when click on refresh button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = [{ id: 1, name: 'Meisam Mofidi', complete: false }];
+const LOCAL_STORAGE_KEY = 'todoslist';
 
 function App() {
-  const [todos, setTodos] = useState(initialState);
+  const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    setTodos(storedTodos);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   const handleAddTodo = (e) => {
     e.preventDefault();
     const name = todoNameRef.current.value;
     if (name === '') return;
 
-    setTodos( preTodo => [...preTodo, { id: uuidv4(), name: name, complete: false }] );
+    setTodos((preTodo) => [
+      ...preTodo,
+      { id: uuidv4(), name: name, complete: false },
+    ]);
     todoNameRef.current.value = null;
   };
 
